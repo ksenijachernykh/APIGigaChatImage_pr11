3b6038f09836593d4fe7e47f640cff31a9d2549e
qwelord <secondofall22@rambler.ru>
2025-12-17 13:34:52 +0500

    Разработанный метод для получения токена пользователя.Черных

--- APIGigaChatImage/Program.cs
+++ APIGigaChatImage/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using APIGigaChatImage.Models.Response;
+using Newtonsoft.Json;
 
 namespace APIGigaChatImage
 {
@@ -14,6 +17,41 @@ namespace APIGigaChatImage
         {
         }
 
+        public static async Task<string> GetToken(string rqUID, string bearer)
+        {
+            string returnToken = null;
+            string url = "https://ngw.devices.sberbank.ru:9WU3/api/v2/oauth";
 
+            using (HttpClientHandler handler = new HttpClientHandler())
+            {
+                handler.ServerCertificateCustomValidationCallback = (message, cert, chain, sslPolicyErrors) => true;
+
+                using (HttpClient client = new HttpClient(handler))
+                {
+                    HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, url);
+
+                    request.Headers.Add("Accept", "application/json");
+                    request.Headers.Add("RqUID", rqUID);
+                    request.Headers.Add("Authorization", $"Bearer {bearer}");
+
+                    var data = new List<KeyValuePair<string, string>>
+            {
+                new KeyValuePair<string, string>("scope", "GIGACHAT_API_PERS")
+            };
+
+                    request.Content = new FormUrlEncodedContent(data);
+                    HttpResponseMessage response = await client.SendAsync(request);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string responseContent = await response.Content.ReadAsStringAsync();
+                        ResponseToken token = JsonConvert.DeserializeObject<ResponseToken>(responseContent);
+                        returnToken = token.access_token;
+                    }
+                }
+            }
+
+            return returnToken;
+        }
     }
 }
