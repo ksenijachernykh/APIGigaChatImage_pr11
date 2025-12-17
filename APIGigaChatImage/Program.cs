cabbaaad3942b20fbffc5d9856a42049c5bc40f9
qwelord <secondofall22@rambler.ru>
2025-12-17 13:24:50 +0500

    Добавьте файлы проекта.

---
+++ APIGigaChatImage/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APIGigaChatImage
+{
+    internal class Program
+    {
+        static string ClientId = "";
+        static string AuthorizationKey = "";
+        static void Main(string[] args)
+        {
+        }
+
+
+    }
+}
